1005680c51b580fb02c303952218408e5bcd1211
phillipstemmlar <phillipstemmlar@gmail.com>
2021-01-27 00:42:23 +0200

    chore: matrix operator methods + math reference pdfs

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,39 +1,44 @@
 #include "pch.h"
 #include "Matrix.h"
-#include <sstream>
 #include <iostream>
 
 //====================STRUCTURE=====================
 
-Matrix::Matrix(unsigned int m, unsigned int n) {
-	//TODO: check edge cases with matrix dimensions
+Matrix::Matrix(int m, int n, bool identify) {
+	if (m < Matrix::MIN_DIMMENSION_SIZE) m = Matrix::MIN_DIMMENSION_SIZE;
+	if (n < Matrix::MIN_DIMMENSION_SIZE) n = Matrix::MIN_DIMMENSION_SIZE;
+	if (m > Matrix::MAX_DIMMENSION_SIZE) m = Matrix::MAX_DIMMENSION_SIZE;
+	if (n > Matrix::MAX_DIMMENSION_SIZE) n = Matrix::MAX_DIMMENSION_SIZE;
 
 	this->m = m;
 	this->n = n;
 
-	this->values = new float* [this->m];
-	for (int j = 0; j < this->m; ++j) {
-		this->values[j] = new float[this->n];
-		for (int i = 0; i < this->n; ++i)  this->values[j][i] = (m == n && i == j) ? 1 : 0;
-	}
+	this->values = std::vector<std::vector<float>>(this->m, std::vector<float>(this->n, 0));
 
+	if (identify && this->isSquare()) {
+		for (int i = 0; i < this->n; ++i)  this->values[i][i] = 1;
+	}
 }
 
 Matrix::~Matrix() {
-	for (int j = 0; j < this->m; ++j) delete[] this->values[j];
-	delete[] this->values;
+	//if (this->values != nullptr) {
+	//	for (int j = 0; j < this->m; ++j) {
+	//		if (this->values[j] != nullptr) delete[] this->values[j];
+	//	}
+	//	delete[] this->values;
+	//}
 }
 
 //====================WRITERS=====================
 
-void Matrix::set(unsigned int j, unsigned int i, float value) {
+void Matrix::set(int j, int i, float value) {
 	if (j >= 0 && j < this->M() && i >= 0 && i < this->N()) this->values[j][i] = value;
 	else throw MatrixIndexOutOfBoundsException();
 }
 
 //====================READERS=====================
 
-float Matrix::get(unsigned int j, unsigned int i) const {
+float Matrix::get(int j, int i) const {
 	if (j >= 0 && j < this->M() && i >= 0 && i < this->N()) return this->values[j][i];
 	else throw MatrixIndexOutOfBoundsException();
 }
@@ -88,10 +93,74 @@ bool Matrix::isPositive() {
 	return true;
 }
 
+//===================COMPARISON====================
+
+bool Matrix::compareSize(Matrix const& mat1, Matrix const& mat2) { return mat1.m == mat2.m && mat1.n == mat2.n; }
+
+bool Matrix::compareValues(Matrix const& mat1, Matrix const& mat2) {
+	if (!Matrix::compareSize(mat1, mat2)) return false;
+	for (int j = 0; j < mat1.m; ++j)
+		for (int i = 0; i < mat1.n; ++i)
+			if (mat1.values[j][i] != mat2.values[j][i]) return false;
+	return true;
+}
+
+bool Matrix::compareSize(Matrix const& mat) { return Matrix::compareSize(*this, mat); }
+bool Matrix::compareValues(Matrix const& mat) { return Matrix::compareValues(*this, mat); }
+
+//===================OPERATORS=======================
+
+Matrix Matrix::Add(const Matrix& mat1, const Matrix& mat2) {
+	Matrix res(mat1.m, mat1.n);
+	for (int j = 0; j < res.m; ++j)
+		for (int i = 0; i < res.n; ++i) res.values[j][i] = mat1.values[j][i] + mat2.values[j][i];
+	return res;
+}
+
+Matrix Matrix::Sub(const Matrix& mat1, const Matrix& mat2) {
+	Matrix res(mat1.m, mat1.n);
+	for (int j = 0; j < res.m; ++j)
+		for (int i = 0; i < res.n; ++i) res.values[j][i] = mat1.values[j][i] - mat2.values[j][i];
+	return res;
+}
+
+Matrix Matrix::Scale(const Matrix& mat, float s) {
+	Matrix res(mat.m, mat.n);
+	for (int j = 0; j < res.m; ++j)		//NOTE: scale each entry in matrix by "s"
+		for (int i = 0; i < res.n; ++i) res.values[j][i] = s * mat.values[j][i];
+	return res;
+}
+
+Matrix Matrix::transpose(Matrix& mat) {
+	Matrix trans(mat.n, mat.m);	//NOTE: swap n and m for transpose
+	for (int j = 0; j < mat.m; ++j)
+		for (int i = 0; i < mat.n; ++i)  trans.values[j][i] = mat.values[i][j];
+	return trans;
+}
+
+
+Matrix Matrix::Add(const Matrix& mat) { return Matrix::Add(*this, mat); }
+Matrix Matrix::Sub(const Matrix& mat) { return Matrix::Sub(*this, mat); }
+Matrix Matrix::Scale(float s) { return Matrix::Scale(*this, s); }
+Matrix Matrix::transpose() { return Matrix::transpose(*this); }
+
+Matrix Matrix::operator+ (const Matrix& mat) { return Matrix::Add(*this, mat); }
+Matrix Matrix::operator- (const Matrix& mat) { return Matrix::Sub(*this, mat); }
+Matrix Matrix::operator* (float s) { return Matrix::Scale(*this, s); }
+
+Matrix operator* (float s, const Matrix& mat) { return Matrix::Scale(mat, s); }
+
+
+
 //====================OUTPUT=====================
 
 void Matrix::print() {
 	std::cout << "( m = " << this->m << ", n = " << this->n << " )" << std::endl;
+
+	//if (this->values == nullptr) {
+	//	std::cout << "[null]" << std::endl;
+	//	return;
+	//}
 
 	for (int j = 0; j < this->m; ++j) {
 		std::cout << "[ ";
--- Matrix.h
+++ Matrix.h
@@ -1,21 +1,22 @@
 #pragma once
 
 #include "math.stem.h"
-#include "MatrixException.h";
+#include "MatrixException.h"
 #include <string>
+#include <vector>
 
 class DLLEXPORT Matrix {
 public:
 
 	//Structure
-	Matrix(unsigned int m, unsigned int n);
+	Matrix(int m, int n, bool identify = true);
 	~Matrix();
 
 	//writers
-	void set(unsigned int j, unsigned int i, float value);
+	void set(int j, int i, float value);
 
 	//readers
-	float get(unsigned int j, unsigned int i) const;
+	float get(int j, int i) const;
 
 	int M() const;
 	int N() const;
@@ -27,18 +28,39 @@ public:
 	bool isZero();			// a(i,j)=0 for all i,j
 	bool isNonNegative(); // a(i,j)>=0 for all i,j
 	bool isPositive();		// a(i,j)>0 for all i,j
+	//bool isInvertible();
 
 	//Comparison
-		//Compare 2 matrices' sizes
-		//Compare 2 matrices' values
+	static bool compareSize(Matrix const& mat1, Matrix const& mat2);
+	static bool compareValues(Matrix const& mat1, Matrix const& mat2);
+	bool compareSize(Matrix const& mat);
+	bool compareValues(Matrix const& mat);
+
 
 	//Operations
 		//Addition (Different forms, all cases)
+			//Matrix-Matrix Addition
+			//Matrix-Matrix Subtraction
 		//multiplication  (Different forms, all cases)
-		//scaling
-		//transpose
+			//Matrix-Scalar Multiplication
+			//Matrix-Matrix Multiplication
+			//vector?...
 		//inverse
+		//determinant
 
+	static Matrix Add(const Matrix& mat1, const Matrix& mat2);
+	static Matrix Sub(const Matrix& mat1, const Matrix& mat2);
+	static Matrix Scale(const Matrix& mat1, float s);
+	static Matrix transpose(Matrix& mat);
+
+	Matrix Add(const Matrix& mat);
+	Matrix Sub(const Matrix& mat);
+	Matrix Scale(float s);
+	Matrix transpose();
+
+	Matrix operator+ (const Matrix& mat);			//Matrix-Matrix Addition
+	Matrix operator- (const Matrix& mat);			//Matrix-Matrix Subtraction
+	Matrix operator* (float s);								//Matrix-Scalar Multiplication
 
 	//output
 	void print();
@@ -47,6 +69,10 @@ private:
 	int m;
 	int n;
 
-	float** values;
+	std::vector<std::vector<float>> values;
 
+	static const int MIN_DIMMENSION_SIZE = 1;
+	static const int MAX_DIMMENSION_SIZE = 10000;
 };
+
+extern DLLEXPORT Matrix operator* (float s, const Matrix& mat); //Scalar-Matrix Multiplication
